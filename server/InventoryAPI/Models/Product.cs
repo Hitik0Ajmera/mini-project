d8471fa6d7070ecc1b18d1c16b3a60470be95fa9
Hitik Patel <hitik.patel@ajmerainfotech.com>
2025-05-27 12:43:42 +0530

    add

---
+++ server/InventoryAPI/Models/Product.cs
@@ -0,0 +1,7 @@
+public class Product {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Category { get; set; }
+    public int Quantity { get; set; }
+    public string? Barcode { get; set; }
+}
